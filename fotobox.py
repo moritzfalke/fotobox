3edb9034ab793772658b40cd2629b4b5b25f9053
Moritz Falke <m.falke@sap.com>
2018-03-09 13:20:21 +0100

    Test Camera and config file

---
+++ fotobox.py
@@ -0,0 +1,44 @@
+import tweepy
+from time import sleep
+import RPi.GPIO as GPIO
+import picamera
+import configparser
+
+# Setup Config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+pin_camera_btn = config['CONFIGURATION']['pin_camera_btn']
+pin_confirm_btn = config['CONFIGURATION']['pin_confirm_btn']
+pin_cancel_btn = config['CONFIGURATION']['pin_cancel_btn']
+prep_delay = config['CONFIGURATION']['prep_delay']
+photo_w = config['CONFIGURATION']['photo_w']
+photo_h = config['CONFIGURATION']['photo_h']
+screen_w = config['CONFIGURATION']['screen_w']
+screen_h = config['CONFIGURATION']['screen_h']
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_camera_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_confirm_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pin_cancel_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+debounce = 0.05  # Min duration (seconds) button is required to be "pressed in" for.
+
+
+# Setup Camera
+
+camera = picamera.PiCamera()
+camera.rotation = 270
+camera.resolution = (photo_h, photo_w)
+camera.hflip = True
+
+# Path
+REAL_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def main():
+    print("startup")
+    print("press the button to take a photo")
+
+    camera.start_preview(resolution=(screen_w,screen_h))
