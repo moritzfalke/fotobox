18f8357f52576dcc2b118e1e077b4d11596c47fd
Moritz Falke <m.falke@sap.com>
2018-03-09 14:32:17 +0100

    Adds Casting to int for Values from config.ini

--- fotobox.py
+++ fotobox.py
@@ -8,14 +8,14 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-pin_camera_btn = config['CONFIGURATION']['pin_camera_btn']
-pin_confirm_btn = config['CONFIGURATION']['pin_confirm_btn']
-pin_cancel_btn = config['CONFIGURATION']['pin_cancel_btn']
-prep_delay = config['CONFIGURATION']['prep_delay']
-photo_w = config['CONFIGURATION']['photo_w']
-photo_h = config['CONFIGURATION']['photo_h']
-screen_w = config['CONFIGURATION']['screen_w']
-screen_h = config['CONFIGURATION']['screen_h']
+pin_camera_btn = int(config['CONFIGURATION']['pin_camera_btn'])
+pin_confirm_btn = int(config['CONFIGURATION']['pin_confirm_btn'])
+pin_cancel_btn = int(config['CONFIGURATION']['pin_cancel_btn'])
+prep_delay = int(config['CONFIGURATION']['prep_delay'])
+photo_w = int(config['CONFIGURATION']['photo_w'])
+photo_h = int(config['CONFIGURATION']['photo_h'])
+screen_w = int(config['CONFIGURATION']['screen_w'])
+screen_h = int(config['CONFIGURATION']['screen_h'])
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
